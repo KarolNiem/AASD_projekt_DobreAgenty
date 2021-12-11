e777e9b16e57724005728b743edcb75d6e5daa87
Krzysztof Łapan <lapankrz@interia.pl>
2021-12-11 15:46:06 +0100

    Added dynamic JADE runtime & agent creation.

--- src/com/dobreagenty/Main.java
+++ src/com/dobreagenty/Main.java
@@ -1,7 +1,33 @@
 package com.dobreagenty;
 
+import jade.wrapper.AgentContainer;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
 public class Main {
 
     public static void main(String[] args) {
+        Profile profile = new ProfileImpl();
+        AgentContainer container = Runtime.instance().createMainContainer(profile);
+        try {
+            AgentController costEvaluator = container.createNewAgent("CostEvaluator",
+                    "com.dobreagenty.agents.CostEvaluatorAgent", null);
+            AgentController usabilityEvaluator = container.createNewAgent("UsabilityEvaluator",
+                    "com.dobreagenty.agents.UsabilityEvaluatorAgent", null);
+            AgentController ageStructEvaluator = container.createNewAgent("AgeStructEvaluator",
+                    "com.dobreagenty.agents.AgeStructEvaluatorAgent", null);
+            AgentController budgetChecker = container.createNewAgent("BudgetChecker",
+                    "com.dobreagenty.agents.BudgetCheckerAgent", null);
+
+            costEvaluator.start();
+            usabilityEvaluator.start();
+            ageStructEvaluator.start();
+            budgetChecker.start();
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
     }
 }
