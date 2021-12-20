d360854fb55d715f5c287593fb7ec48b169e2d6e
Karol Niemczycki <karolniemczycki98@gmail.com>
2021-12-20 15:13:01 +0100

    customer behaviour added

--- src/com/dobreagenty/behaviours/CustomerBehaviour.java
+++ src/com/dobreagenty/behaviours/CustomerBehaviour.java
@@ -5,6 +5,63 @@ import jade.core.behaviours.CyclicBehaviour;
 public class CustomerBehaviour extends CyclicBehaviour {
     @Override
     public void action() {
+            public ArrayList<Idea> ideas = new ArrayList<>();
+    public ArrayList<CustomerDetails> customers = new ArrayList<>();
 
+    @Override
+    public void action() {
+        try {
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                System.out.println("Customer received: " + msg.getContent());
+                String senderName = msg.getSender().getName();
+                if (senderName=="CustomerHandler") handleCustomerHandlerMessage(msg);
+                else handleApplicationMessage(msg);
+            } else {
+                block();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void handleCustomerHandlerMessage(ACLMessage msg) {
+        String content = msg.getContent();
+
+        //Nie jestem pewien tak tego stringa/jsona przesłać do aplikacji
+        /*ACLMessage newMsg = new ACLMessage(ACLMessage.REQUEST);
+        newMsg.addReceiver(new AID("Application", AID.ISLOCALNAME));
+        newMsg.setContent(content);
+        myAgent.send(newMsg);
+        ???*/
+    }
+
+    public void handleApplicationMessage(ACLMessage msg) {
+        String content = msg.getContent(); //zakładam, że dane z aplikacji otrzymujemy jako zbiorczy string
+        //zawierający dane o kliencie i pomyśle. Format stringa to ''objectData-customerData''
+        String[] parts = content.split("-");
+        String object=parts[0];
+        String user=parts[1];
+        JSONObject jsonObject = new JSONObject(object);
+        JSONObject jsonUser = new JSONObject(user);
+
+        Idea idea = new Idea(jsonObject);
+        ideas.add(idea);
+        CustomerDetails customerDetails = new CustomerDetails(jsonUser);
+        customers.add(customerDetails);
+
+        ACLMessage newMsgIdea = new ACLMessage(ACLMessage.REQUEST);
+        newMsgIdea.addReceiver(new AID("CustomerHandler", AID.ISLOCALNAME));
+        newMsgIdea.setContent(object);
+        myAgent.send(newMsgIdea);
+
+        ACLMessage newMsgUser = new ACLMessage(ACLMessage.REQUEST);
+        newMsgUser.addReceiver(new AID("CustomerHandler", AID.ISLOCALNAME));
+        newMsgUser.setContent(user);
+        myAgent.send(newMsgUser);
+
+        //Nie wiem jak się zachowa program kiedy wysyłamy szeregowo 2 różne wiadomości do tego samego odbiorcy
+        //dlatego alternatywnie można będzie przesłać cały ''content'' do customerHandlera i dopiero tam
+        //rozbić go na 2 stringi
+    }
     }
 }
--- src/com/dobreagenty/payloads/Idea.java
+++ src/com/dobreagenty/payloads/Idea.java
@@ -5,12 +5,27 @@ import com.dobreagenty.misc.OfferType;
 
 public class Idea {
     public String name;
-    public OfferType offerType;
+    public OfferTypeEnum offerType;
     public DistrictEnum district;
 
-    public Idea(String name, OfferType offerType, DistrictEnum district) {
+    public Idea(String name, OfferTypeEnum offerType, DistrictEnum district) {
         this.name = name;
         this.offerType = offerType;
         this.district = district;
+    }
+
+    public Idea(JSONObject json) {
+        name = json.getString("name");
+        offerType = OfferTypeEnum.values()[json.getInt("type")];
+        district = DistrictEnum.values()[json.getInt("district")];
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put("name", name);
+        json.put("type", offerType);
+        json.put("district", district);
+        return json.toString();
     }
 }
