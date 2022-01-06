3fe87feaa12026366a1007675894acb668f6a2bf
Kamil W <kamkam@onet.eu>
2022-01-07 00:46:45 +0100

    Check for empty fields in questionnaire and idea creator views. Fill comboboxes in idea creator view

--- src/main/java/com/dobreagenty/aasd_javafx_demo/ApplicationController.java
+++ src/main/java/com/dobreagenty/aasd_javafx_demo/ApplicationController.java
@@ -1,21 +1,92 @@
 package com.dobreagenty.aasd_javafx_demo;
 
+import com.dobreagenty.misc.DistrictEnum;
+import com.dobreagenty.misc.OfferTypeEnum;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ApplicationController {
+
+
 
     private Stage stage;
     private Scene scene;
     private Parent root;
     private FXMLLoader fxmlLoader;
+
+    @FXML private TextField questionnaireName;
+    @FXML private TextField questionnaireSurname;
+    @FXML private TextField questionnaireMail;
+    @FXML private TextField questionnairePhone;
+    @FXML private Label questionnaireEmptyError;
+
+    @FXML private TextField ideaName;
+    @FXML private ComboBox<String> ideaType;
+    @FXML private ComboBox<String> ideaDistrict;
+    @FXML private Label ideaEmptyError;
+
+    public void initializeIdeaTypeComboBox(){
+        ArrayList<String> typeList = new ArrayList<String>();
+        for(var value : OfferTypeEnum.values()){
+            typeList.add(value.toString());
+        }
+        ideaType.getItems().setAll(typeList);
+    }
+
+    public void initializeIdeaDistrictComboBox(){
+        ArrayList<String> districtList = new ArrayList<String>();
+        for(var value : DistrictEnum.values()){
+            districtList.add(value.toString());
+        }
+        ideaDistrict.getItems().setAll(districtList);
+    }
+
+    public void handleQuestionnaireConfirmButton(ActionEvent event){
+        if (!questionnaireName.getText().isEmpty() || !questionnaireSurname.getText().isEmpty() || !questionnaireMail.getText().isEmpty() || !questionnairePhone.getText().isEmpty()) {
+            try{
+                switchToIdeaCreatorView(event);
+            }
+            catch(IOException e)
+            {
+                System.err.println(e);
+                e.printStackTrace();
+            }
+        }
+        else {
+            questionnaireEmptyError.setText("Nie wypełniono wszystkich pól!");
+        }
+    }
+
+    public void handleIdeaConfirmButton(ActionEvent event) {
+        if (!ideaName.getText().isEmpty()) {
+            try{
+                switchToLoadingScreenView(event);
+            }
+            catch(IOException e)
+            {
+                System.err.println(e);
+                e.printStackTrace();
+            }
+        }
+        else {
+            ideaEmptyError.setText("Nie wypełniono wszystkich pól!");
+        }
+    }
 
     public void switchToHelloView(ActionEvent event) throws IOException {
         fxmlLoader = new FXMLLoader(ApplicationMain.class.getResource("/view/hello-view.fxml"));
