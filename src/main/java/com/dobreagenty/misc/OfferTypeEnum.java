97a497d6d537e1a2183febf9c8cca6aefdf83385
Kamil W <kamkam@onet.eu>
2022-01-07 22:27:36 +0100

    Add polish translation for idea types

--- src/main/java/com/dobreagenty/misc/OfferTypeEnum.java
+++ src/main/java/com/dobreagenty/misc/OfferTypeEnum.java
@@ -9,4 +9,18 @@ public enum OfferTypeEnum {
     BusStop,
     TrashCan,
     Library;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case School -> "Szkoła";
+            case Playground -> "Plac zabaw";
+            case Preschool -> "Przedszkole";
+            case BrineGraduationTower -> "Tężnia solankowa";
+            case ParkingLot -> "Parking";
+            case BusStop -> "Przystanek autobusowy";
+            case TrashCan -> "Śmietnik";
+            case Library -> "Biblioteka";
+        };
+    }
 }
